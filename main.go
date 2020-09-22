c8910ae75670fd77a486577a53666c13f1702f38
vceballos1978 <vladimir.ceballos@gmail.com>
2020-09-22 13:24:08 -0500

    api add

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -22,17 +24,38 @@ var tasks = allTask{
 		ID:       1,
 		NOMBRE:   " VLADIMIR",
 		APELLIDO: "CEBALLOS",
-		EMAIL:    "VLADIMIR.CEBALLOS",
+		EMAIL:    "VLADIMIR.CEBALLOS@gmail.com",
 	},
 }
 
+func getTaks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprint(w, "Insert a valid data")
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+
+}
 func indexRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Wecome the my GO API! VLADIMIR CEBALLOS")
+	fmt.Fprintf(w, "Wecome the my GO API! VLADIMIR CEBALLOS THE BEST DEVELOPER")
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-
+	router.HandleFunc("/tasks", getTaks).Methods("GET")
 	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", createTask).Methods("POST")
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
